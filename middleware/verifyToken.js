613823dc1959895582ab2dbc64fe559cf0a71810
bdushime <bdushime47@gmail.com>
2025-12-23 23:43:23 +0200

    checkin functionality

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,7 +4,7 @@ const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token || req.headers.authorization;
 
     if (authHeader) {
-       
+    
         const token = authHeader.split(" ")[1]; 
 
         jwt.verify(token, process.env.JWT_SECRET || "mySuperSecretKey123", (err, user) => {
