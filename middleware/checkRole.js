6f3cd32e5d18a5a4fbc092b9fb8b6325223eb1d7
bdushime <bdushime47@gmail.com>
2026-01-02 22:46:52 +0200

    Fix User model

--- middleware/checkRole.js
+++ middleware/checkRole.js
@@ -5,7 +5,7 @@ const checkRole = (allowedRoles) => {
         // req.user is already set by verifyToken middlewar
         if (!req.user || !allowedRoles.includes(req.user.role)) {
             return res.status(403).json({ 
-                message: "Access Denied: You do not have permission to perform this action.(allowed)" 
+                message: "Access Denied: You do not have permission to perform this action." 
             });
         }
         next();
