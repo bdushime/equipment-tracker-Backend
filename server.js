45856ca008b847952c5df5ce819c5c924ccb2aee
bdushime <bdushime47@gmail.com>
2026-01-23 13:50:16 +0200

    adding iot.js

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.use('/api/data', require('./routes/data'));
 app.use('/api/monitoring', require('./routes/monitoring'));
 app.use('/api/config', require('./routes/config'));
 app.use('/api/notifications', notificationRoute);
+app.use('/api/iot', iotRoute);
 
 
 const connectDB = async () => {
